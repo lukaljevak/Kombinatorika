f9fae58a9349e377d3b97dbd48a78a0a2e290182
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-12-15 10:16:41 +0100

    Add files via upload

---
+++ Kombinatorika/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kombinatorika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var permutations = GetPermutations(digits, digits.Length);
+
+            foreach (var perm in permutations)
+            {
+                var numberArray = perm.ToArray();
+                int number = int.Parse(string.Join("", numberArray));
+                if (IsValid(numberArray))
+                {
+                    Console.WriteLine("Tražen broj je:" + number);
+                    Console.ReadLine();
+                    return;
+                   
+                }
+            }
+            Console.ReadKey();
+        }
+        static IEnumerable<IEnumerable<T>> GetPermutations<T>(T[] list, int length)
+        {
+            if (length == 1)
+                return list.Select(t => new T[] { t });
+
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(e => !t.Contains(e)),
+                            (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+        static bool IsValid(int[] numberArray)
+        {
+            
+            for (int i = numberArray.Length; i > 0; i--)
+            {
+                int currentNumber = int.Parse(string.Join("", numberArray.Take(i)));
+                if (currentNumber % i != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
